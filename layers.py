79085c383cc07ace4162de97eab3e87227d1d6a2
RakitinDen <rakitindenis32@gmail.com>
2019-12-03 18:27:56 +0300

    add decent g-crf realization

--- layers.py
+++ layers.py
@@ -5,12 +5,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 
-from KroneckerProduct import KroneckerProduct
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class ConjugateGradients: # Модуль для решения СЛУ методом сопряженных градиентов
-    def __init__(self, shift=0, max_iter=500, tolerance=1e-4):
+    def __init__(self, shift=0, max_iter=500, tolerance=1e-3):
         self.shift = shift
         self.max_iter = max_iter
         self.tolerance = tolerance
@@ -50,18 +48,6 @@ class ConjugateGradients: # Модуль для решения СЛУ метод
 
 def dense_gaussian_crf(solver):
     class DenseGaussianCRF(torch.autograd.Function):
-        @staticmethod
-        def expand_matrix(mtx):                 # Функция, переводящая матрицу A в матрицу 
-                                                # T_n,n (I x A^T) = (A^T x I)T_d,n
-            batch_size = mtx.shape[0]           # нужную для вычисления градиента
-            N = mtx.shape[2]
-            d = mtx.shape[1]
-
-            A = mtx
-            C = torch.diag_embed(A[:, None, :, :], dim1=2, dim2=1)
-            D = C.view(batch_size, -1, 1, N * d)
-            E = D.repeat_interleave(N, dim=0)
-            return E.view(batch_size, -1, N * d)
 
         @staticmethod
         def forward(ctx, embedding, unary):
@@ -79,15 +65,9 @@ def dense_gaussian_crf(solver):
 
             dL_dB = solver.solve(A, dL_dx)
 
-            dL_dATA = KroneckerProduct(dL_dB.shape[1:], x.shape[1:]).cuda()(dL_dB, x)
-            dL_dATA = torch.transpose(dL_dATA, 1, 2)
-
-            id_n = torch.eye(A.shape[2], device=device)
-            dATA_dA = KroneckerProduct(id_n.shape, AT.shape[1:]).cuda()(id_n[None, :, :], AT)
-            dATA_dA += DenseGaussianCRF.expand_matrix(A)
-
-            dL_dA = -torch.matmul(dL_dATA, dATA_dA)
-            dL_dA = dL_dA.view(A.shape)
+            dL_dA = torch.matmul(torch.matmul(A, dL_dB), torch.transpose(x, 1, 2))
+            dL_dA += torch.matmul(torch.matmul(A, x), torch.transpose(dL_dB, 1, 2))
+            dL_dA = -dL_dA
 
             return dL_dA, dL_dB
 
--- tests.py
+++ tests.py
@@ -7,17 +7,17 @@ import torch.optim
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def optimization_test(n_epochs=3, shift=0.01):
-    A = torch.normal(mean=0, std=0.1, size=(10, 3, 5), device=device, requires_grad=True)
-    B = torch.normal(mean=0, std=0.1, size=(10, 5, 1), device=device, requires_grad=True)
+def optimization_test(n_epochs=10, shift=0.01):
+    A = torch.normal(mean=0, std=0.1, size=(10, 128, 321 * 321), device=device, requires_grad=True)
+    B = torch.normal(mean=0, std=0.1, size=(10, 321 * 321, 1), device=device, requires_grad=True)
     dcrf = dense_gaussian_crf(ConjugateGradients(shift))
-    opt = torch.optim.Adam([A, B], lr=3e-4)
+    opt = torch.optim.Adam([A, B], lr=3e-2)
     criterion = nn.MSELoss()
 
     for i in range(n_epochs):
         opt.zero_grad()
         x = dcrf(A, B)
-        loss = criterion(x, torch.ones_like(x))
+        loss = criterion(x, torch.zeros_like(x))
         loss.backward()
         print(loss)
         opt.step()
