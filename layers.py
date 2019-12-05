63d5d9b4874cb945a357ba1052dcf77788d630b7
Denis Rakitin <rakitindenis32@yandex.ru>
2019-12-05 23:43:35 +0300

    update potts-type gradients

--- layers.py
+++ layers.py
@@ -12,11 +12,14 @@ class ConjugateGradients: # Модуль для решения СЛУ метод
         self.shift = shift
         self.max_iter = max_iter
         self.tolerance = tolerance
+        self.type = 'default'
+
 
     def matrix_product(self, A, AT, x): # вычисление матричного произведения 
         A_x = torch.matmul(A, x)        # для матрицы вида A^T * A + lambda * I
         ATA_x = torch.matmul(AT, A_x) + self.shift * x
         return ATA_x
+
 
     def solve(self, embedding, unary):
         x_0 = torch.normal(mean=0, std=0.1, size=unary.shape, device=device)
@@ -55,6 +58,7 @@ def dense_gaussian_crf(solver):
             ctx.save_for_backward(embedding, x)
             return x
 
+
         @staticmethod
         def backward(ctx, grad_output):
             A, x = ctx.saved_tensors
@@ -72,4 +76,87 @@ def dense_gaussian_crf(solver):
             return dL_dA, dL_dB
 
     return DenseGaussianCRF.apply
+
+
+class PottsTypeConjugateGradients:
+    def __init__(self, shift=0, max_iter=500, tolerance=1e-3):
+        self.shift = shift
+        self.max_iter = max_iter
+        self.tolerance = tolerance
+        self.type = 'potts'
+
+
+    def matrix_product(self, A, AT, p, batch_size):
+        P = p.reshape(batch_size, -1, A.shape[2])
+        Q_1 = torch.matmul(A, torch.transpose(P, 1, 2))
+
+        q_sum_1 = Q_1.sum(dim=-1).unsqueeze(2)
+        q_sum_2 = torch.matmul(torch.transpose(A, 1, 2), q_sum_1)
+
+        Q = q_sum_2 + self.shift * torch.transpose(P, 1, 2) - torch.matmul(torch.transpose(A, 1, 2), Q_1)
+        return torch.transpose(Q, 1, 2).reshape(batch_size, -1, 1)
+
+
+    def solve(self, embedding, unary):
+        x_0 = torch.normal(mean=0, std=0.1, size=unary.shape, device=device)
+        A = embedding
+        B = unary
+        batch_size = A.shape[0]
+
+        AT = torch.transpose(A, 1, 2)
+        x_k = x_0.clone()
+
+        r_k = B - self.matrix_product(A, AT, x_k, batch_size)
+        p_k = r_k.clone()
+
+        for k in range(self.max_iter):
+            r_kTr_k = torch.matmul(torch.transpose(r_k, 1, 2), r_k)
+            ATAp_k = self.matrix_product(A, AT, p_k, batch_size)
+            alpha_k = r_kTr_k / torch.matmul(torch.transpose(p_k, 1, 2), ATAp_k)
+            x_k += alpha_k * p_k
+            r_k_new = r_k - alpha_k * ATAp_k
+
+            if torch.max(r_k_new) < self.tolerance:
+                break
+
+            beta_k = torch.matmul(torch.transpose(r_k_new, 1, 2), r_k_new) / r_kTr_k
+            p_k = r_k_new + beta_k * p_k
+            r_k = r_k_new.clone()
+
+        return x_k
+
+def potts_type_crf(solver):
+    class PottsTypeCRF(torch.autograd.Function):
+
+        @staticmethod
+        def forward(ctx, embedding, unary):
+            x = solver.solve(embedding, unary)
+            ctx.save_for_backward(embedding, x)
+            return x
+
+        @staticmethod
+        def backward(ctx, grad_output):
+            A, x = ctx.saved_tensors
+            P = A.shape[2]
+            L = x.shape[1] // P
+
+            AT = torch.transpose(A, 1, 2)
+            dL_dx = grad_output
+            if len(dL_dx.shape) == 2:
+                dL_dx = dL_dx.unsqueeze(0)
+
+            dL_dB = solver.solve(A, dL_dx)
+
+            dL_dB_x = torch.matmul(dL_dB, torch.transpose(x, 1, 2))
+
+            F = torch.zeros(P * (L - 1) + 1, P * (L - 1) + 1, device=device)
+
+            idxs = torch.arange(P * (L - 1) + 1, device=device) % P == 0
+            F[idxs, idxs] = 1
+
+            dL_dATA = F.conv2d(dL_dB_x.unsqueeze(1), F.unsqueeze(0)[None, :, :, :]).squeeze(1)
+
+            dL_dA = -torch.matmul(A, dL_dATA + torch.transpose(dL_dATA, 1, 2))
+
+            return dL_dA, dL_dB
 
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from layers import ConjugateGradients, dense_gaussian_crf
+from layers import ConjugateGradients, PottsTypeConjugateGradients, dense_gaussian_crf
 
 import numpy as np
 import torch
@@ -10,10 +10,10 @@ import models
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def conjgrad_test(shift=0.01, max_iter=500, tolerance=1e-4, batch_size=10, d=128, H = 50, W = 50):
+def conjgrad_test(shift=0.01, max_iter=500, tolerance=1e-4, batch_size=10, d=128, H = 50, W = 50, L=1):
     solver = ConjugateGradients(shift=shift, max_iter=max_iter, tolerance=tolerance)
-    A = torch.normal(mean=0, std=0.1, size=(batch_size, d, H * W), device=device)
-    B = torch.normal(mean=0, std=0.1, size=(batch_size, H * W, 1), device=device)
+    A = torch.normal(mean=0, std=0.1, size=(batch_size, d, H * W * L), device=device)
+    B = torch.normal(mean=0, std=0.1, size=(batch_size, H * W * L, 1), device=device)
     x_opt = solver.solve(A, B)
 
     ATA_I = torch.matmul(torch.transpose(A, 1, 2), A) + shift * torch.eye(H * W, device=device)[None, :, :]
@@ -22,8 +22,35 @@ def conjgrad_test(shift=0.01, max_iter=500, tolerance=1e-4, batch_size=10, d=128
     log_str = "Shift %.7g, tol %.7g, H %d W %d" %(shift, tolerance, H, W)
     assert nn.MSELoss()(B, B_opt) < tolerance, log_str
 
-    torch.cuda.empty_cache()
     print('ConjGrad Success')
+
+
+def expand_matrix(A, shift, L):
+    res = torch.zeros(A.shape[0], A.shape[2] * L, A.shape[2] * L, device=device)
+    ATA = torch.matmul(torch.transpose(A, 1, 2), A)
+    for i in range(L - 1):
+        for j in range(i + 1, L):
+            k = A.shape[2]
+            res[:, (i + 1) * k:(i + 2) * k, (j - 1) * k:j * k] = ATA
+            res[:, (j - 1) * k:j * k, (i + 1) * k:(i + 2) * k] = ATA
+
+    res += shift * torch.eye(res.shape[1], device=device)[None, :, :]
+    return res
+
+
+def potts_conjgrad_test(shift=0.01, max_iter=500, tolerance=1e-4, batch_size=10, d=128, H = 50, W = 50, L=2):
+    solver = PottsTypeConjugateGradients(shift=shift, max_iter=max_iter, tolerance=tolerance)
+    A = torch.normal(mean=0, std=0.1, size=(batch_size, d, H * W), device=device)
+    B = torch.normal(mean=0, std=0.1, size=(batch_size, H * W * L, 1), device=device)
+    x_opt = solver.solve(A, B)
+
+    big_A = expand_matrix(A, shift, L)
+
+    B_opt = torch.matmul(big_A, x_opt)
+    log_str = "Shift %.7g, tol %.7g, H %d W %d L %d" %(shift, tolerance, H, W, L)
+    assert nn.MSELoss()(B, B_opt) < tolerance * 10, log_str
+
+    print('PottsConjGrad Success')
 
 
 def optimization_test(batch_size=10, d=128, H = 50, W = 50, n_epochs=200, shift=0.01):
@@ -62,13 +89,13 @@ def check_unary_prototypes(model, batch_size, n_labels, H, W):
     print(out.shape)
     assert out.shape == (batch_size, n_labels, down_size(H), down_size(W))
 
+
 def check_pairwise_prototypes(model, batch_size, n_labels, H, W, embedding_size):
     X = torch.zeros(batch_size, 3, H, W, device=device)
     out = model(X)
 
     print(out.shape)
     assert out.shape == (batch_size, embedding_size, n_labels, down_size(H), down_size(W))
-
 
 
 def run_layers_tests():
@@ -83,5 +110,4 @@ def run_layers_tests():
 
 
 if __name__ == '__main__':
-    model = models.ResNetPW(3, 128, [3, 4, 23, 3], 21).to(device)
-    check_pairwise_prototypes(model, 3, 21, 321, 321, 128)
+    run_layers_tests()
